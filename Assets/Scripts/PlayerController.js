6349466b7b309836e1ff81ca0efc8739f0379440
高橋 啓治郎 <keijiro@Keijiros-MacBook-Air.local>
2011-09-19 23:05:56 +0900

    効果音の付け忘れを対処。

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -1,31 +1,42 @@
-// プレイヤーのキャラクターコントローラーを制御するスクリプト
+// プレイヤーのキャラクターコントローラーを制御するスクリプト。
 
-var walkSpeed : float = 3.0;      // 歩く速度
-var gravity : float = 20.0;       // 重力加速度
-var jumpSpeed : float = 8.0;      // ジャンプの初速 
+var walkSpeed : float = 3.0;      // 歩く速度。
+var gravity : float = 20.0;       // 重力加速度。
+var jumpSpeed : float = 8.0;      // ジャンプの初速。 
 
-private var velocity : Vector3;   // 現在の速度
+var jumpSE : AudioClip;		// ジャンプの効果音。
+var touchSE : AudioClip;	// 着地の効果音。
+
+private var velocity : Vector3;		// 現在の速度。
+private var wasGrounded : boolean;	// 前のフレームで地面に接していたか？
 
 function Start() {
-	// 歩行アニメーションを若干小走りにする
+	// 歩行アニメーションを若干小走りにする。
 	animation["Walk"].speed = 2.0;
 }
 
 function Update() {
 	var controller : CharacterController = GetComponent(CharacterController);
 	
-	// 接地中のみ行う処理
+	// 接地中のみ行う処理。
 	if (controller.isGrounded) {
-		// キー入力から速度を決める
+		// 前フレームで地面に接していなかった場合は着地処理を行う。
+		if (!wasGrounded) {
+			animation.Play("Crouch");
+			audio.PlayOneShot(touchSE);
+		}
+		
+		// キー入力から速度を決める。
 		velocity = Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		velocity *= walkSpeed;
 		
 		if (Input.GetButtonDown("Jump")) {
-			// ジャンプ開始。縦方向の初速を与えてジャンプを再生する
+			// ジャンプ開始。縦方向の初速を与えてジャンプを再生する。
 			velocity.y = jumpSpeed;
 			animation.Play("Jump");
+			audio.PlayOneShot(jumpSE);
 		} else if (velocity.magnitude > 0.5) {
-			// 歩行アニメーションに切り替えつつ、進行方向に旋回する
+			// 歩行アニメーションに切り替えつつ、進行方向に旋回する。
 			animation.CrossFade("Walk", 0.1);
 			transform.LookAt(transform.position + velocity);
 		} else {
@@ -33,9 +44,12 @@ function Update() {
 		}
 	}
 	
-	// 重力による下方向への加速
+	// 接地フラグを保存する。
+	wasGrounded = controller.isGrounded;
+	
+	// 重力による下方向への加速。
 	velocity.y -= gravity * Time.deltaTime;
 	
-	// キャラクターコントローラーの移動
+	// キャラクターコントローラーの移動。
 	controller.Move(velocity * Time.deltaTime);
 }
